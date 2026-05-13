4aae9c3a5f3f01029c919e60bd3e95e706ef5263
saraprabs <vsaranya04@gmail.com>
2026-05-13 11:51:07 +0200

    test results added

--- Backend/tests/unit/test_book.py
+++ Backend/tests/unit/test_book.py
@@ -22,3 +22,20 @@ def test_book_toggle_favorite():
     # Toggle back to False
     book.is_favorite = not book.is_favorite
     assert book.is_favorite is False
+
+"""
+collected 6 items                                                                                                                                                                  
+
+tests\\integration\\test_bookstore.py ....                                                                                                                                     [ 66%]
+tests\\unit\\test_book.py ..                                                                                                                                                   [100%]
+
+===================================================================================== PASSES ======================================================================================
+============================================================================= short test summary info =============================================================================
+PASSED tests/integration/test_bookstore.py::test_add_book_increases_count
+PASSED tests/integration/test_bookstore.py::test_toggle_favorite_by_id
+PASSED tests/integration/test_bookstore.py::test_get_only_favorites
+PASSED tests/integration/test_bookstore.py::test_toggle_non_existent_book_raises_error
+PASSED tests/unit/test_book.py::test_book_initialization
+PASSED tests/unit/test_book.py::test_book_toggle_favorite
+================================================================================ 6 passed in 0.10s ================================================================================
+"""
